3e0aeac25f00d1181d8e97c08edea96b38a0d616
Sitrakiniaina <Sitrakaa9@gmail.com>
2023-10-12 14:04:19 +0300

    HOMEPAGE

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,10 @@
 import './css/App.css'
 import './icons/icons.css'
 import './custom-element'
-import {enabledRoute} from "./js/route-enabled.js";
+import { enabledRoute } from "./js/route-enabled.js";
 import "./js/multiple-upload.js";
 // Custom react element
-import './js/react-custom-element'
+import './js/react-custom-element';
+import './carousel'
 
 enabledRoute()
